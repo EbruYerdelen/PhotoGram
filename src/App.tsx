fcabe386870a65a0eed9c22c401100739acbe929
Ebru <yerdebru@gmail.com>
2024-06-12 01:24:02 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {Routes , Route} from "react-router-dom"
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages"; //You imported home as a name import because later on,we'll import home,page,..bla
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
+import AuthLayout from "./_auth/AuthLayout";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/*public routes written individually as route which are routes everyone can see like sign in/up page*/}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/*private routes are routes which users see after sign in/up*/
+        /*Here index means that our app starts with home page so its path is "/"*/}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+}
+
+export default App
---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Outlet, Navigate } from "react-router-dom"
+
+//here, when you try to write the path of SignupForm in the web page,you'll see the AuthLayout,because
+//in this component you still haven't returned the child pages withing the component,you can achieve this by Outlet component of react router dom
+const AuthLayout = () => {
+  const isAuthenticated = false;
+  return (
+    <div>
+      {
+        isAuthenticated ? (
+          <Navigate to="/" />
+        ) :
+          (
+            <>
+              <section className='flex flex-1 justify-center items-center flex-col py-10'>
+                <Outlet/>
+              </section>
+
+              <img 
+                src='/assets/images/side-img.svg'
+                alt='logo'
+                className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'/>
+            </>
+          )
+      }
+    </div>
+  )
+}
+
+export default AuthLayout
